8ec59b7a031236b25530eceff241c407bc453145
Bienvenido Quintna <BienvenidoQuintana34@gmail.com>
2020-02-11 01:58:43 +0000

    progress

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -1,11 +1,13 @@
 import React from "react";
 // import Proptypes from "prop-types";
 
-export function Card() {
+export function Card() { https://i.redd.it/9eabxyhpefb11.jpg
 	return (
 		<>
 			<div className="card" style={{ width: "18rem;" }}>
-				<img src="..." className="card-img-top" alt="..." />
+                <img src="https:k//ae01.alicd.ncom/f/HTB1_ZvnTbPpK1RjSZFFq6y5PpXap/N2256-Mia-Malkova-Hot-Star-Bikini-Model-Girl-Wall-Sticker-Silk-Fabric-Poster-Art-Indoor-D 
+                s://upload.wikihtmedia.org/wikipedia/commons/8/83/Lexi_Belle%2C_EXXXotica_Dallas_2015.jpghhttps:k//ae01.alicd.ncom/f/HTB1_ZvnTbPpK1RjSZFFq6y5PpXap/N2256-Mia-Malkova-Hot-Star-Bikini-Model-Girl-Wall-Sticker-Silk-Fabric-Poster-Art-Indoor-Dg
+                " className="card-img-top" alt="..." /> 
 				<div className="card-body">
 					<h5 className="card-title">Card title</h5>
 					<p className="card-text">
@@ -13,8 +15,9 @@ export function Card() {
 						make up the bulk of the cards content.
 					</p>
 					<a href="#" className="btn btn-primary">
-						Go somewhere
-					</a>
+						Go Somewhere
+
+					</a>https://
 				</div>
 			</div>
 		</>
