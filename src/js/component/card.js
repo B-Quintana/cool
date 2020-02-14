5a8973e2511fc4a843ffbec99704c301381fe427
Bienvenido Quintna <BienvenidoQuintana34@gmail.com>
2020-02-14 00:46:22 +0000

    update

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -6,11 +6,11 @@ export function Card() {
 	return (
 		<>
 			<div className="card" style={{ width: "18rem;" }}>
-https://i.pinimg.com/474x/97/1c/22/971c2282a34ef7351eac07016a7a2b0b.jpg
+
+                <img src = https://i.pinimg.com/474x/97/1c/22/971c2282a34ef7351eac07016a7a2b0b.jpg
 https://i.redd.it/h677ldhjmbp31.jpg
 https://images-na.ssl-images-amazon.com/images/I/61dDgJExUAL._SY550_.jpg
 https://i.ebayimg.com/images/g/cFMAAOSwQYZWt5KZ/s-l1600.jpg
-                <img src = 
 
                 // 	imgUrl: PropTypes.string,
 // 	title: PropTypes.string
