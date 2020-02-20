6024dbaa276d17dab80db0fb467613d6bdeaf2ea
Bienvenido Quintna <BienvenidoQuintana34@gmail.com>
2020-02-20 17:31:20 +0000

    update

--- src/js/component/jumboton.js
+++ src/js/component/jumboton.js
@@ -1,4 +1,11 @@
-https://i.pinimg.com/474x/97/1c/22/971c2282a34ef7351eac07016a7a2b0b.jpg
-https://i.redd.it/h677ldhjmbp31.jpg
-https://images-na.ssl-images-amazon.com/images/I/61dDgJExUAL._SY550_.jpg
-https://i.ebayimg.com/images/g/cFMAAOSwQYZWt5KZ/s-l1600.jpg
+import React from "react";
+
+
+            export function from jumboton(<div class="jumbotron">
+  <h1 class="display-4">Hello, world!</h1>
+  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+  <hr class="my-4">
+  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+</div>){
+
