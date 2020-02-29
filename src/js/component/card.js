15883798161169ec9599812de81f49f5d6cf208d
Bienvenido Quintna <BienvenidoQuintana34@gmail.com>
2020-02-29 01:34:04 +0000

    update

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -2,6 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 
 export function Card(props) {
+    let list = {
+        pic1: "https://images-na.ssl-images-amazon.com/images/I/61dDgJExUAL._SY550_.jpg",
+        pic2: "https://i.ebayimg.com/images/g/cFMAAOSwQYZWt5KZ/s-l1600.jp"
+        pic3: "https://img.ifunny.co/images/57d245e4a6e7be1099e29faf31e62950686156c37a296ea149e75b704cb8aae0_1.jpg"
+        pic4: "https://i.pinimg.com/originals/a3/51/43/a351438d3fbdafe93ea04ed78dea6a36.jpg,"
+        pic5: "https://img.ifunny.co/images/57d245e4a6e7be1099e29faf31e62950686156c37a296ea149e75b704cb8aae0_1.jpg "
+    }
 	return (
 		<>
 			<div className="card-deck" style={{ width: "300px" }}>
@@ -22,8 +29,7 @@ export function Card(props) {
 	);
 }
 
-Card.PropTypes = {
-	myimage: https://images-na.ssl-images-amazon.com/images/I/61dDgJExUAL._SY550_.jpg  https://i.ebayimg.com/images/g/cFMAAOSwQYZWt5KZ/s-l1600.jpg https://i.pinimg.com/originals/bc/97/e2/bc97e255bab5dc0f610e273332658314.jpg  https://img.ifunny.co/images/57d245e4a6e7be1099e29faf31e62950686156c37a296ea149e75b704cb8aae0_1.jpg 
-                   https://i.pinimg.com/originals/a3/51/43/a351438d3fbdafe93ea04ed78dea6a36.jpg https://www.xcritic.com/newswire/wp-content/uploads/2019/10/1500x500-copy-2.jpg
+Card.PropTypes
+      myimage  
+                  
 
-};   
