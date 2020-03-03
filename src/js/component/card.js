395c72167e129bd3f2fb93644a81a8e93eee2457
Bienvenido Quintna <BienvenidoQuintana34@gmail.com>
2020-03-03 01:59:01 +0000

    update

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -4,14 +4,14 @@ import PropTypes from "prop-types";
 export function Card(props) 
     
 
-        "https://images-na.ssl-images-amazon.com/images/I/61dDgJExUAL._SY550_.jpg"
+       let va= ["https://images-na.ssl-images-amazon.com/images/I/61dDgJExUAL._SY550_.jpg",
         "https://i.ebayimg.com/images/g/cFMAAOSwQYZWt5KZ/s-l1600.jpg",
     
         "https://img.ifunny.co/images/57d245e4a6e7be1099e29faf31e62950686156c37a296ea149e75b704cb8aae0_1.jpg",
         "https://i.pinimg.com/originals/a3/51/43/a351438d3fbdafe93ea04ed78dea6a36.jpg",
          "https://www.xcritic.com/newswire/wp-content/uploads/2019/10/1500x500-copy-2.jpg",
           "https://i.redd.it/tnrurew3fww31.png",
-         "https://www.xcritic.com/newswire/wp-content/uploads/2019/10/1500x500-copy-2.jpg"
+         "https://www.xcritic.com/newswire/wp-content/uploads/2019/10/1500x500-copy-2.jpg"]
 
 	return (
 		<>-
