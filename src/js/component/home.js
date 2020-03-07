7f6228b1d642affbb9bb2be7988a6a500c59ac41
Bienvenido Quintna <BienvenidoQuintana34@gmail.com>
2020-03-07 00:49:06 +0000

    update

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -4,6 +4,7 @@ import { Navbar } from "./navbar";
 import { Cards } from "./cards";
 
 let va = [
+	"https://cdn.wallpapersafari.com/55/61/qwADp2.jpg",
 	"https://images-na.ssl-images-amazon.com/images/I/61dDgJExUAL._SY550_.jpg",
 	"https://i.ebayimg.com/images/g/cFMAAOSwQYZWt5KZ/s-l1600.jpg",
 	"https://img.ifunny.co/images/57d245e4a6e7be1099e29faf31e62950686156c37a296ea149e75b704cb8aae0_1.jpg",
